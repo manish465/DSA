efcb212efb030966191a83948fa463e42319e5ab
manish <46858945+manish465@users.noreply.github.com>
2022-09-27 18:36:13 +0530

    added selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sort(new int[]{5, 4, 3, 2, 1,0,-1})));
+    }
+
+    public static int[] sort(int[] arr){
+        for(int i=0;i < arr.length - 1;i++){
+            int minIdx = i;
+
+            for(int j=i+1;j<arr.length;j++)
+                if(arr[minIdx] > arr[j])
+                    minIdx = j;
+
+            swap(arr, i, minIdx);
+        }
+
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int minIdx) {
+        int temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+    }
+}
